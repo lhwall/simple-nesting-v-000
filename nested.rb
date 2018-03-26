083ab246300b87fad5b9bce9604d747bd6e0d212
Lucy Wall <lhwall@gmail.com>
2018-03-26 15:31:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].to_s.chomp
+     programmer_hash[:dennis_ritchie][:languages].join.chomp
 end
 
 def adding_matz
